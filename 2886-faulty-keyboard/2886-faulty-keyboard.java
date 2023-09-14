ea5d59b9f5924c324bc52d840ce425246baba7d6
Sumeet Wadile <sumeetwd25@gmail.com>
2023-09-15 00:09:21 +0530

    Time: 11 ms (18.28%), Space: 44.4 MB (12.84%) - LeetHub

--- 2886-faulty-keyboard/2886-faulty-keyboard.java
+++ 2886-faulty-keyboard/2886-faulty-keyboard.java
@@ -4,11 +4,9 @@ class Solution {
         String s1="";
         for(int i=0; i<s.length(); i++){
             if(s.charAt(i)=='i'){
-                String s2 = "";
-                for(int j=s1.length()-1; j>=0; j--){
-                    s2+=s1.charAt(j);
-                }
-                s1=s2;
+
+                StringBuilder s2 = new StringBuilder(s1).reverse();
+                s1=s2.toString();
             }
             else{
                 s1+=s.charAt(i);
