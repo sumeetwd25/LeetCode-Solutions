c82731a0df8766b51a5c9d35d66ac7067020eaca
Sumeet Wadile <sumeetwd25@gmail.com>
2023-11-10 00:35:40 +0530

    Time: 10 ms (65.2%), Space: 44 MB (66.8%) - LeetHub

--- 1885-count-number-of-homogenous-substrings/1885-count-number-of-homogenous-substrings.java
+++ 1885-count-number-of-homogenous-substrings/1885-count-number-of-homogenous-substrings.java
@@ -2,16 +2,16 @@ class Solution {
     public int countHomogenous(String s) {
         int left = 0;
         long res = 0;
-        
+
         for (int right = 0; right < s.length(); right++) {
             if (s.charAt(left) == s.charAt(right)) {
                 res += right - left + 1;
-            } else {
+            }
+            else {
                 res += 1;
                 left = right;
             }
         }
-
-        return (int) (res % (1000000007));       
+        return (int) (res % (1000000007));
     }
 }
